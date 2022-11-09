75dcd9ae981d7f0e062023b259eae98d271427e2
allansmar <111984890+allansmar@users.noreply.github.com>
2022-11-09 11:45:43 -0300

    Add files via upload

---
+++ 5_funções_091122.py
@@ -0,0 +1,14 @@
+'''
+FUNÇÕES
+variaveis Locais
+'''
+
+def calcularmedia(nota1,nota2):
+    media = (nota1+nota2)/2
+    return media
+
+aluno1 = calcularmedia(6,5)
+aluno2 = calcularmedia(10,2)
+
+print(aluno1)
+print(aluno2)
