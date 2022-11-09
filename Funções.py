2c645a4a4fddf288d208b86d99979a047c7a7a90
allansmar <111984890+allansmar@users.noreply.github.com>
2022-11-09 10:05:22 -0300

    Add files via upload

---
+++ Funções.py
@@ -0,0 +1,19 @@
+'''def calculamedia(num1,num2):
+    media = (num1+num2)/2
+    print(f"a média é {media}")
+
+calculamedia(5,5)'''
+
+#Peça o nome do usuario
+# Criar uma função que de voas vindas ao usuario
+# A funcao deve ter um parametro
+
+'''def bemvindo():
+    nome = input("Diga seu nome: ")
+    print(f"{nome} Seja bem vindo!")
+bemvindo()'''
+'''def bemvindo(parametro):
+    print(f"Olá, {parametro}")
+nome = input("Diga seu nome: ")
+bemvindo(nome)'''
+
