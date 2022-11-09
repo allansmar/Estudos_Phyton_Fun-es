efd22e16bbd01369af18c3906d90896dbb48f3d9
allansmar <111984890+allansmar@users.noreply.github.com>
2022-11-09 10:05:48 -0300

    Add files via upload

---
+++ 2_Funções_091122.py
@@ -0,0 +1,9 @@
+def calcmedia (num1,num2):
+    media = ((num1+num2)/2)
+    print(media)
+
+for i in range(1):
+    numero1 = int(input(f'Digite um numero: '))
+    numero2 = int(input(f'Digite outro numero: '))
+
+calcmedia(numero1,numero2)
